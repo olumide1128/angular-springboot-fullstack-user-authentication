a1bf0580c89f1443c7fa707aed0ae9f2d70dbc72
Olumide Oyegoke <59411943+olumide1128@users.noreply.github.com>
2023-02-04 13:34:26 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'userAuth_with_Angular_and_springboot';
+}
